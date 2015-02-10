404ea9e732b93c191e633efe4faffa13325eed8e
Nathan Crews <n_crews@hotmail.com>
2015-02-09 22:33:46 -0600

    update drupal2.sh

--- drupal2.sh
+++ drupal2.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-
+#
 ###############################
-## Drupal Initialization Script
-## this time taking a different route
-## download composer and drush to 
-## hopefully automate creating drupal
-## installitions from simple make files.
-##
+# Drupal Initialization Script
+# this time taking a different route
+# download composer and drush to 
+# hopefully automate creating drupal
+# instillations from simple make files.
+#
 
-## Get composer.. This may be useful for more than just drupal
-## i may need to move this into the bootstrap script(s)
+# Get composer.. This may be useful for more than just drupal
+# I may need to move this into the bootstrap script(s)
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 ## now go and get Drush current version 6.x
 composer global require drush/drush:6.*
+
+sudo ln -s /home/vagrant/.composer/vendor/bin/drush /usr/local/bin/drush
