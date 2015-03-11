03b8d89db623a7b905de318d64f6c6d97335e428
Nathan Crews <n_crews@hotmail.com>
2015-03-11 12:50:29 -0500

    nodejs
    adding node update to bottom of node install script.

--- nodejs.sh
+++ nodejs.sh
@@ -6,12 +6,6 @@
 #sudo apt-get install nodejs npm
 
 
-## Updating Node to latest version
-# using instructions found at http://davidwalsh.name/upgrade-nodejs
-
-#sudo npm cache clean -f 
-#sudo npm install -g n
-#sudo n stable
 
 ## Changing this again to follow the instructions at
 ## https://github.com/joyent/node/wiki/installing-node.js-via-package-manager
@@ -25,3 +19,9 @@ sudo apt-get install -y nodejs
 # sudo npm install --no-bin-links --no-bin-links  <name of module to install >
 # use --no-bin-links to prevent symlink issues on vagrant/windows.
 
+## Updating Node to latest version
+# using instructions found at http://davidwalsh.name/upgrade-nodejs
+
+sudo npm cache clean -f 
+sudo npm install -g n
+sudo n stable
