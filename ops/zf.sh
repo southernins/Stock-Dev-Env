e7922982452a376f9b16cdf62a1f30cda376f086
Nathan Crews <n_crews@hotmail.com>
2015-03-22 00:03:48 -0500

    zend framework
    Working on Zend Framework Project install/setup

--- ops/zf.sh
+++ ops/zf.sh
@@ -7,4 +7,21 @@
 # http://framework.zend.com/manual/2.0/en/user-guide/skeleton-application.html
 #
 
-php composer.phar create-project --repository-url="http://packages.zendframework.com" zendframework/skeleton-application /vagrant
+# Since we cannot currently install into a populated directory
+# we'll use a temp dir to hold the project.
+mkdir /home/vagrant/zf
+
+# PHP Update....
+
+sudo apt-get install python-software-properties
+sudo apt-get update
+sudo apt-get install php5
+
+sudo service apache2 restart
+
+composer create-project --repository-url="http://packages.zendframework.com" zendframework/skeleton-application /home/vagrant/zf
+
+rm -rf README.md /home/vagrant/zf/vendor/bin/ /vagrant/public/
+
+
+mv * /vagrant/
