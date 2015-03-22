8283bcf215cda2db136892ea07f566198a0a7579
Nathan Crews <n_crews@hotmail.com>
2015-03-22 00:36:32 -0500

    Zend Framework
    Updating ZF install script

--- ops/zf.sh
+++ ops/zf.sh
@@ -14,14 +14,22 @@ mkdir /home/vagrant/zf
 # PHP Update....
 
 sudo apt-get install python-software-properties
+
+sudo add-apt-repository ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install php5
 
-sudo service apache2 restart
 
+
+# Install Zend Framework Skeleton via Composer
 composer create-project --repository-url="http://packages.zendframework.com" zendframework/skeleton-application /home/vagrant/zf
 
+# Clean up some directories.
 rm -rf README.md /home/vagrant/zf/vendor/bin/ /vagrant/public/
 
-
+# Move 
 mv * /vagrant/
+
+rm -rf /var/www/html
+sudo ln -s /vagrant/public /var/www/html
+sudo service apache2 restart
