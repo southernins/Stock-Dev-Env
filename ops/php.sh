888af085295bda91470fe266140df796bb4b413c
Nathan Crews <n_crews@hotmail.com>
2015-04-10 13:49:37 -0500

    add curl to php
    include php5-curl in php install.

--- ops/php.sh
+++ ops/php.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install PHP and modules
-sudo apt-get -y install php5-mysql php5 php5-gd
+sudo apt-get -y install php5-mysql php5 php5-gd php5-curl
 
 # Edit Config to set Timezone in apache config file
 sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php5/apache2/php.ini
