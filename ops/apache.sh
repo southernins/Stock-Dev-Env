1815db60ec36f8fba0dcb29513c3f9621748cb62
Nathan Crews <n_crews@hotmail.com>
2015-08-05 11:54:49 -0500

    apache envvars
    Update script to copy envvars file rather than MOVE it.  this prevents
    problems during a re-provision.

--- ops/apache.sh
+++ ops/apache.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install  apache2
 
 if [ ! -h /var/www ];
-then 
+then
     sudo rm -rf /var/www/html
     sudo ln -s /vagrant/public /var/www/html
 
@@ -13,9 +13,9 @@ then
 	# Use sed to add AllowOverride All to default config.
 	sudo sed -i '/DocumentRoot \/var\/www\/html/c DocumentRoot \/var\/www\/html \n\n\<Directory \/var\/www\/html \>\nAllowOverride All\n \<\/Directory\> ' /etc/apache2/sites-available/000-default.conf
 
-	sudo mv /vagrant/env_config/envvars /etc/apache2/envvars
+	sudo /bin/cp -rf /vagrant/env_config/envvars /etc/apache2/envvars
 	sudo chown vagrant /var/lock/apache2
     sudo usermod -a -G adm vagrant
-	
+
     sudo service apache2 restart
 fi
