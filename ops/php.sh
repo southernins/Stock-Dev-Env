4e222262743fa33fc7ea09299c4651d8e4f73a14
Nathan Crews <n_crews@hotmail.com>
2015-10-26 10:32:33 -0500

    resolve php.sh confilict
    Updated php.sh manually from dev version to resolve confilct

--- ops/php.sh
+++ ops/php.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-sudo apt-get -y install php5-mysql php5 php5-gd
-=======
 # Install PHP and modules
 sudo apt-get -y install php5-mysql php5 php5-gd php5-curl php5-json
 
@@ -11,4 +8,3 @@ sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php5/ap
 
 # udpate time zone in CLI config file.
 sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php5/cli/php.ini
->>>>>>> refs/remotes/origin/Development
