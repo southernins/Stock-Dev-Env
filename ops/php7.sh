60edd6b363637d423f41e7010dd5372946ef99d5
Nathan Crews <nbcrews@gmail.com>
2017-01-18 16:40:40 -0600

    Add php7 script

---
+++ ops/php7.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+
+
+# Install PHP
+# sudo apt-get -y install php5-mysql php5 php5-gd php5-curl php5-json
+sudo apt-get -y install php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0 
+
+# Install PHP Modules
+sudo apt-get -y install php7.0-mysql php7.0-fpm php7.0-gd php7.0-curl php-mcrypt php7.0-mbstring
+
+# Edit Config to set Timezone in apache config file
+sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php/7.0/apache2/php.ini
+
+# update time zone in CLI config file.
+sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php/7.0/cli/php.ini
+
+# PS1="React-Form: \w\$ "
