d0b07d19af3dcb5c74d6f43fc4caf9c883b4672b
awrightsouthern <awright@southerninsurance.net>
2017-07-05 14:57:51 -0500

    Create dusk-dependencies.sh

---
+++ ops/dusk-dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+###############################
+# Scripts to run after you vagrant ssh into a new box for the first time
+#
+
+sudo apt-get install -y libnss3-dev libgconf-2-4
+sudo apt-get install chromium-browser
+sudo apt-get install -y xvfb
