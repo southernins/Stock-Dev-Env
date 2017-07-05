76586b4169ae47dd1b3bdda4bd2cea2e38aa2bfb
awrightsouthern <awright@southerninsurance.net>
2017-07-05 14:59:39 -0500

    Create dusk-dependencies.sh

---
+++ ops/dusk-dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+###############################
+# Stuff to install so that laravel dusk will work
+#
+
+sudo apt-get install -y libnss3-dev libgconf-2-4
+sudo apt-get install chromium-browser
+sudo apt-get install -y xvfb
