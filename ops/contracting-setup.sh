b2e7051813f2a068da5d90582eda2c4ebcc5e3b1
awrightsouthern <awright@southerninsurance.net>
2017-07-05 14:08:28 -0500

    Update contracting-setup.sh

--- ops/contracting-setup.sh
+++ ops/contracting-setup.sh
@@ -1,4 +1,9 @@
-# Call and run scripts
+#!/usr/bin/env bash
+#
+###############################
+# Scripts to run after you vagrant ssh into a new box for the first time
+#
+
 ./apache.sh
 ./php7.sh
 ./mysql.sh
