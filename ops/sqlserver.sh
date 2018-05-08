23da53182a087990b05d16ec62787f588efe32ed
Nathan Crews <ncrews@southerninsurance.net>
2018-05-08 15:16:18 -0500

    update sqlserver install script to include proper list/key

--- ops/sqlserver.sh
+++ ops/sqlserver.sh
@@ -25,7 +25,15 @@ sudo /opt/mssql/bin/mssql-conf setup
 # 
 
 # Install the SQL Server command-line tools
+
+# Import the public repository GPG keys:
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Register the Microsoft Ubuntu repository.
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
 # run the installation command with the unixODBC developer package.
+sudo apt-get update
 sudo apt-get install mssql-tools unixodbc-dev
 
 # Add /opt/mssql-tools/bin/ to your PATH environment variable in a bash shell.
