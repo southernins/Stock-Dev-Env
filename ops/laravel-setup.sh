7f8bd0d08054216e2d67fcf55ad171c1a8f1b74f
ncrewsSouthern <ncrews@southerninsurance.net>
2018-12-15 15:57:39 -0600

    adds script to setup everything for laravel

---
+++ ops/laravel-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+###############################
+# Scripts to run after you vagrant ssh into a new box for the first time
+#
+
+sudo rm -rf /vagrant/.git
+
+./apache.sh
+./php7_1.sh
+./mysql.sh
+./composer.sh
+./nodejs.sh
+./db.sh
