40487d8c8d1bfe3a33aa4efa5c5fd685c3f4be81
cfoustsouthern <47403029+cfoustsouthern@users.noreply.github.com>
2019-12-04 15:44:04 -0600

    Create php7_2.sh

---
+++ ops/php7_2.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+
+
+# Install PHP
+# sudo apt-get -y install php5-mysql php5 php5-gd php5-curl php5-json
+sudo apt-get -y install php7.2-cli php7.2-common libapache2-mod-php7.2 php7.2
+
+# Install PHP Modules
+sudo apt-get -y install php7.2-mysql php7.2-fpm php7.2-gd php7.2-curl php7.2-mcrypt php7.2-mbstring php7.2-xml php-xdebug
+
+# Edit Config to set Timezone in apache config file
+sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php/7.2/apache2/php.ini
+
+# update time zone in CLI config file.
+sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php/7.2/cli/php.ini
+
+# Enable xDebug Remote Debugging
+# sudo sed -i "$ xdebug.remote_enable = 1" /etc/php/7.0/mods-available/xdebug.ini
+
+# PS1="React-Form: \w\$ "
