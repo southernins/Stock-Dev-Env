55071d11a91e754b11b87378dc6a52479e20c6b9
awrightsouthern <awright@southerninsurance.net>
2021-03-12 09:04:13 -0600

    Update dusk-dependencies.sh

--- ops/dusk-dependencies.sh
+++ ops/dusk-dependencies.sh
@@ -5,6 +5,8 @@
 #
 
 sudo apt-get install -y libnss3-dev libgconf-2-4
-sudo apt-get install chromium-browser
-sudo apt-get install -y xvfb
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
 sudo apt-get -y install xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
