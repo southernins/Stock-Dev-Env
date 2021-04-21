e118670de22e53e8d00519bab98246a92988bc68
ncrewsSouthern <ncrews@southerninsurance.net>
2021-04-21 14:05:25 -0500

    set explicit version

--- ops/php7_4.sh
+++ ops/php7_4.sh
@@ -9,13 +9,13 @@
 
 # Install PHP
 # sudo apt-get -y install php5-mysql php5 php5-gd php5-curl php5-json
-sudo apt-get -y install php-cli php-common libapache2-mod-php php
+sudo apt-get -y install php7.4-cli php7.4-common libapache2-mod-php php7.4
 
 # Install PHP Modules
-sudo apt-get -y install php-mysql php-fpm php-gd php-curl php-mbstring php-xml php-xdebug php-sqlite
+sudo apt-get -y install php7.4-mysql php7.4-fpm php7.4-gd php7.4-curl php7.4-mbstring php7.4-xml php7.4-xdebug php7.4-sqlite
 
 # install PHP-zip extension for Facebook/WebDriver dependency of laravel/dusk
-sudo apt-get -y install php-zip
+sudo apt-get -y install php7.4-zip
 
 # Edit Config to set Timezone in apache config file
 sudo sed -i "/date.timezone \=/c date.timezone \= America\/Chicago" /etc/php/7.4/apache2/php.ini
